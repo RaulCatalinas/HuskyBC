e106395e97c8c774abf1b9ce32563c13070fa571
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-03-23 01:53:42 +0100

    feat: enable CLI execution from Node.js
    - Fixed issues preventing the CLI from running correctly within a Node.js environment.
    - Now the CLI executes as expected when called from Node.js.

--- npm-package/index.js
+++ npm-package/index.js
@@ -4,22 +4,40 @@ const { spawn } = require('child_process');
 const path = require('path');
 const os = require('os');
 
-
 const binName = {
-  darwin: 'husky-cli-macos',
-  linux: 'husky-cli-linux',
-  win32: 'husky-cli-windows.exe'
-}
+  win32: {
+    x64: 'HuskyBC_windows_amd64_v1/huskybc.exe',
+    arm64: 'HuskyBC_windows_arm64/huskybc.exe',
+  },
+  darwin: {
+    x64: 'HuskyBC_darwin_amd64_v1/huskybc',
+    arm64: 'HuskyBC_darwin_arm64/huskybc',
+  },
+  linux: {
+    x64: 'HuskyBC_linux_amd64_v1/huskybc',
+    arm64: 'HuskyBC_linux_arm64/huskybc',
+  },
+};
 
 const platform = os.platform();
+const arch = os.arch();
 
+// Verify if the platform is supported
 if (!binName[platform]) {
-  console.error('Unsupported platform');
+  console.error(`Platform not supported: ${platform}`);
   process.exit(1);
 }
 
-const binPath = path.join(__dirname, 'bin', binName[platform]);
-const args = process.argv.slice(2);
+// Verify if the architecture is supported
+if (!binName[platform][arch]) {
+  console.error(`Architecture ${arch} not supported for platform ${platform}`);
+  process.exit(1);
+}
+
+const binPath = path.join(__dirname, 'bin', binName[platform][arch]);
+
+// Process arguments to remove additional spaces
+const args = process.argv.slice(2).map(arg => arg.trim());
 
 const proc = spawn(binPath, args, { stdio: 'inherit' });
 
