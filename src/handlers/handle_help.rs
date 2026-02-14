09b5c1d58712a308ca9ffed9ebb8054d032a9511
Raul Catalinas <raulcatalinas@gmail.com>
2026-02-15 00:53:44 +0100

    feat: implement CLI help output

--- src/handlers/handle_help.rs
+++ src/handlers/handle_help.rs
@@ -1,5 +1,15 @@
-use crate::types::CommandResult;
+use crate::{constants::COMMANDS, types::CommandResult};
 
 pub fn handle_help(_: &[String]) -> CommandResult {
+    println!("Usage: huskybc [options]\n");
+    println!("Command line for easy Husky configuration\n");
+
+    for cmd in COMMANDS {
+        println!(
+            "  {}, {}: {}",
+            cmd.full_command, cmd.command_alias, cmd.description
+        );
+    }
+
     Ok(())
 }
